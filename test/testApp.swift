9d88ed394daa268fc51df42859b14cbe8aad6c86
Kanykei B <kanykeiburkhan@gmail.com>
2024-07-17 10:27:37 -0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Kanykei Burkhan on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
